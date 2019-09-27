48e435571acf95687bcaa6bb045d62109191eca4
Minus <altirtertyo@gmail.com>
2019-09-27 15:04:46 +0200

    another update, bruh

--- js/elemHacks.js
+++ js/elemHacks.js
@@ -2,3 +2,9 @@ var menuDiv = document.getElementById("menu");
 
 var editorDiv = document.getElementById("editorDiv");
 
+var modeDiv = document.getElementById("modeDiv");
+
+function setMode(number)
+{
+	modeDiv.scrollTop = parseInt(number)*342;
+};
